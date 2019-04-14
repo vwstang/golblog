6a7ae500734e8cabde4879ca56b2e0cc9c1c3f95
vwstang <vwstang6@gmail.com>
2019-04-14 19:49:33 -0400

    Fixed blog card abnormally large title overflow

--- src/components/BlogCards.js
+++ src/components/BlogCards.js
@@ -2,10 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const BlogCards = (props) => {
+  const { postID, postBG, postTitle } = props;
+  let title = "";
+
+  postTitle.length > 120 ?
+    title = `${postTitle.slice(0, 117)}...` :
+    title = postTitle;
+
   return (
-    <li style={{"backgroundImage":`url(${props.postBG})`}} className="blogCard">
-      <Link className="blogCard__link" to={`/post/${props.postID}`}>
-        <span className="blogCard__title">{props.postTitle}</span>
+    <li style={{"backgroundImage":`url(${postBG})`}} className="blogCard">
+      <Link className="blogCard__link" to={`/post/${postID}`}>
+        <span className="blogCard__title">{title}</span>
       </Link>
     </li>
   )
